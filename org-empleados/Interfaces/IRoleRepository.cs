084de6c44bb5c92044e1f81ffb4999f8b12740a5
ConanGH-S <blandon0207s@gmail.com>
2024-05-28 17:24:12 -0500

    Add: Role repository logic

---
+++ org-empleados/Interfaces/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using org_empleados.Models;
+
+namespace org_empleados.Interfaces
+{
+    public interface IRoleRepository
+    {
+        Task<List<Role>> ListAll();
+        Task<Role?> ListOne(int id);
+        Task<bool> Create(Role role);
+        Task<Role> Update(Role actualRole, Role newRole);
+        Task<Role> Delete(Role role);
+    }
+}
---
+++ org-empleados/Repositories/RoleRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using org_empleados.Data;
+using org_empleados.Interfaces;
+using org_empleados.Models;
+
+namespace org_empleados.Repositories
+{
+    public class RoleRepository(ApplicationDbContext context) : IRoleRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<bool> Create(Role role)
+        {
+            await _context.Roles.AddAsync(role);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Role> Delete(Role role)
+        {
+            role.DeletedAt = DateTime.Now;
+            role.UpdatedAt = DateTime.Now;
+            await _context.SaveChangesAsync();
+            return role;
+        }
+
+        public async Task<List<Role>> ListAll()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        public async Task<Role?> ListOne(int id)
+        {
+            return await _context.Roles.FirstOrDefaultAsync(d => d.Id == id);
+        }
+
+        public async Task<Role> Update(Role actualRole, Role newRole)
+        {
+            actualRole.Name = newRole.Name;
+            actualRole.UpdatedAt = DateTime.Now;
+            await _context.SaveChangesAsync();
+            return actualRole;
+        }
+    }
+}
