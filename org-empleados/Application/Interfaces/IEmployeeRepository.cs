cd267deac60bab91ec10ae5829f6fa0d1f91d7fe
ConanGH-S <blandon0207s@gmail.com>
2024-05-29 13:26:48 -0500

    Remove: db migration

---
+++ org-empleados/Application/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace org_empleados.Application.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<string> ListAll();
+        Task<bool> ListOne();
+        Task<string> Create();
+    }
+}
---
+++ org-empleados/Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using org_empleados.Application.Interfaces;
+using org_empleados.Application.Lib;
+using org_empleados.Domain.Models;
+
+namespace org_empleados.Infrastructure.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public Task<string> Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ListOne()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
